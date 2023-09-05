3e8e33dec5e854b02d038c1f679d7bd3256f8737
djgamertri <95003267+djgamertri@users.noreply.github.com>
2023-09-05 17:53:02 -0500

    Update Pay.routes.js

--- src/routes/Pay.routes.js
+++ src/routes/Pay.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { GetPay, GetPays, NewPay } from "../controllers/Pay.controller.js";
+import { GetPay, GetPays, NewPay, UpdatePay } from "../controllers/Pay.controller.js";
 
 const router = Router()
 
@@ -8,5 +8,7 @@ router.post("/Pay", NewPay);
 router.get("/Pay", GetPay);
 
 router.get("/Pays", GetPays);
+
+router.patch("/Pay", UpdatePay);
 
 export default router
