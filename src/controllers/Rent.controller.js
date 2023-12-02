d12d63fde79607c9a54028bf865031f0e0001e91
Fernando <andresfernandoxd1591@gmail.com>
2023-12-02 13:08:42 -0500

    change query's from rent controller

--- src/controllers/Rent.controller.js
+++ src/controllers/Rent.controller.js
@@ -2,7 +2,7 @@ import { conex } from '../../db/db.js'
 
 export const GetRentPending = async (req, res) => {
   try {
-    const [result] = await conex.query('select u.NameUser, c.NameCommonArea, r.RentDate from users as u JOIN rent as r on u.IdUser = r.IdUser JOIN commonarea as c on c.IdCommonArea = r.IdCommonArea JOIN PayAdmin pa ON u.IdUser = pa.IdUser WHERE pa.StatusPayAdmin = 1 AND r.status = 0;')
+    const [result] = await conex.query('select r.IdRent, u.NameUser, c.NameCommonArea, r.RentDate, r.active from users as u JOIN rent as r on u.IdUser = r.IdUser JOIN commonarea as c on c.IdCommonArea = r.IdCommonArea JOIN PayAdmin pa ON u.IdUser = pa.IdUser WHERE pa.StatusPayAdmin = 1 AND r.status = 0;')
     console.log(result)
     res.json(result)
   } catch (error) {
@@ -16,7 +16,7 @@ export const GetRentPending = async (req, res) => {
 
 export const GetRentAccepted = async (req, res) => {
   try {
-    const [result] = await conex.query('select u.NameUser, c.NameCommonArea, r.RentDate from users as u JOIN rent as r on u.IdUser = r.IdUser JOIN commonarea as c on c.IdCommonArea = r.IdCommonArea JOIN PayAdmin pa ON u.IdUser = pa.IdUser WHERE pa.StatusPayAdmin = 1 AND r.status = 1;')
+    const [result] = await conex.query('select r.IdRent, u.NameUser, c.NameCommonArea, r.RentDate, r.active from users as u JOIN rent as r on u.IdUser = r.IdUser JOIN commonarea as c on c.IdCommonArea = r.IdCommonArea JOIN PayAdmin pa ON u.IdUser = pa.IdUser WHERE pa.StatusPayAdmin = 1 AND r.status = 1;')
     console.log(result)
     res.json(result)
   } catch (error) {
